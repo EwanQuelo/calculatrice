b9935f3504d6ec05f84acd5d52c295cf229dece9
EwanQuelo <156847481+EwanQuelo@users.noreply.github.com>
2024-02-18 00:15:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("resultat");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function viderDisplay(){
+    display.value = ".";
+}
+
+function calculer(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "*Invalide*";
+    }
+}
